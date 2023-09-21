3688989b8fc9e4f86f569ee1061bc2aa73d30668
jasper123pyah <jasper.vanden.meiracker@indicia.nl>
2023-09-21 14:41:11 +0200

    Init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <img src={"/webyx.svg"}/>
+      <h1>Hello! We are currently still under construction.</h1>
+      <h2>Check back soon!</h2>
+      <h3>
+        To learn more about me you can visit <a href="https://www.jaspervandenmeiracker.com/">my personal website</a>.
+      </h3>
+    </>
+  )
+}
+
+export default App
